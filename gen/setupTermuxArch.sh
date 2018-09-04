84450d2987ae1163d2334dd99cda704b85179590
sdrausty <sdrausty@gmail.com>
2018-09-04 11:09:38 -0400

    Commit on 20180904 branch master 161195434907

--- gen/setupTermuxArch.sh
+++ gen/setupTermuxArch.sh
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6 id206714411697"
+versionid="gen.v1.6 id161195434907"
 ## INIT FUNCTIONS ##############################################################
 aria2cif() { 
 	dm=aria2c
@@ -578,21 +578,21 @@ _TRPEXIT_() { # Run on exit.
 	local RV="$?"
 	rm -rf "$TAMPDIR"
 	sleep 0.04
-	CDIRS=( bin boot dev etc home lib mnt opt proc root run sbin srv sys tmp usr var )
-	CDIRSV="0"
- 	for i in "${CDIRS[@]}" ; do
-		if $(ls -A $INSTALLDIR/$i 2>/dev/null)
-	then
-			CDIRSV="1"
-		fi
-		if [[ "$CDIRSV" = 1 ]] ; then
-			break
-		fi
-	done
-	if [[ "$CDIRSV" = 0 ]] ; then
-		rmdir $TAMPDIR
-		rmdir $INSTALLDIR
-	fi
+# 	CDIRS=( bin boot dev etc home lib mnt opt proc root run sbin srv sys tmp usr var )
+# 	CDIRSV="0"
+#  	for i in "${CDIRS[@]}" ; do
+# 		if $(ls -A $INSTALLDIR/$i 2>/dev/null)
+# 	then
+# 			CDIRSV="1"
+# 		fi
+# 		if [[ "$CDIRSV" = 1 ]] ; then
+# 			break
+# 		fi
+# 	done
+# 	if [[ "$CDIRSV" = 0 ]] ; then
+# 		rmdir $TAMPDIR
+# 		rmdir $INSTALLDIR
+# 	fi
 	if [[ "$RV" = 0 ]] ; then
 		printf "\\a\\e[0;32m%s %s \\a\\e[0m$versionid\\e[1;34m: \\a\\e[1;32m%s\\e[0m\\n\\n\\a\\e[0m" "${0##*/}" "$args" "DONE 🏁 "
 		printf "\\e]2; %s: %s \\007" "${0##*/} $args" "DONE 🏁 "
@@ -648,7 +648,7 @@ declare CPUABI7="armeabi-v7a"
 declare CPUABI8="arm64-v8a"
 declare CPUABIX86="x86"
 declare CPUABIX86_64="x86_64"
-declare DFL="/gen" # Used for development 
+declare DFL="/gen"	## Used for development 
 declare dm="wget"	## download manager
 declare DMVERBOSE="-q"	## -v for verbose download manager output from curl and wget;  for verbose output throughout runtime also change in `setupTermuxArchConfigs.sh` when using `setupTermuxArch.sh manual`. 
 declare	ed=""
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="v1.6 id9579"
+versionid="gen.v1.6 id161195434907"
 ## INIT FUNCTIONS ##############################################################
 aria2cif() { 
 	dm=aria2c
