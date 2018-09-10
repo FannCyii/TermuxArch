982f79af3503d45ebc9bf89e0ace274ee5246036
sdrausty <sdrausty@gmail.com>
2018-09-10 09:09:33 -0400

    Commit on 20180910 branch master 37782645446423

--- archlinuxconfig.sh
+++ archlinuxconfig.sh
@@ -350,7 +350,6 @@ addkeys() {
 	_CFLHDR_ root/bin/keys 
 	cat >> root/bin/keys <<- EOM
 	declare -a KEYRINGS
-	T=0.4
 
 	_TRPET_() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -359,17 +358,18 @@ addkeys() {
 	}
 	
 	_GENEN_() { # This for loop generates entropy on device for \$t seconds.
-		n=2 # Number of loop generations for generating entropy.
-		t=256 # Maximum number of seconds loop shall run unless keys completes earlier.
-		for i in "\$(seq 1 "\$n")"; do
-			"\$(nice -n 20 find / -type f -exec cat {} \; >/dev/null 2>/dev/null & sleep "\$T" ; kill \$! 2>/dev/null)" 2>/dev/null &
-			sleep "\$T"
-			"\$(nice -n 20 ls -alR / >/dev/null 2>/dev/null & sleep "\$T" ; kill \$! 2>/dev/null)" 2>/dev/null &
-			sleep "\$T"
-			"\$(nice -n 20 find / >/dev/null 2>/dev/null & sleep "\$T" ; kill \$! 2>/dev/null)" 2>/dev/null &
-			sleep "\$T"
-			"\$(nice -n 20 cat /dev/urandom >/dev/null 2>/dev/null & sleep "\$T" ; kill \$! 2>/dev/null)" 2>/dev/null &
-			sleep "\$T"
+		N=2 # Number of loop generations for generating entropy.
+		T0=256 # Maximum number of seconds loop shall run unless keys completes earlier.
+		T1=0.4
+		for I in "\$(seq 1 "\$N")"; do
+			"\$(nice -n 20 find / -type f -exec cat {} \; >/dev/null 2>/dev/null & sleep "\$T0" ; kill \$! 2>/dev/null)" 2>/dev/null &
+			sleep "\$T1"
+			"\$(nice -n 20 ls -alR / >/dev/null 2>/dev/null & sleep "\$T0" ; kill \$! 2>/dev/null)" 2>/dev/null &
+			sleep "\$T1"
+			"\$(nice -n 20 find / >/dev/null 2>/dev/null & sleep "\$T0" ; kill \$! 2>/dev/null)" 2>/dev/null &
+			sleep "\$T1"
+			"\$(nice -n 20 cat /dev/urandom >/dev/null 2>/dev/null & sleep "\$T0" ; kill \$! 2>/dev/null)" 2>/dev/null &
+			sleep "\$T1"
 		done
 	}
 
