c1d301952a0a948a898916faaed39fe140894d27
sdrausty <sdrausty@gmail.com>
2018-09-11 18:04:56 -0400

    Commit on 20180911 branch master 53474872844701

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6.id617124059073"
+versionid="v1.6.id6397"
 ## INIT FUNCTIONS ##############################################################
 aria2cif() { 
 	dm=aria2c
